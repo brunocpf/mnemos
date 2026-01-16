e1332564dd72ddcaacfe0ed7199a32ced880fa44
Bruno Fernandes <brunocpf@outlook.com>
2026-01-16 15:47:01 -0300

    feat: add offline layout and page components for improved user experience

---
+++ src/app/[locale]/~offline/layout.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable check-file/folder-naming-convention */
+import type { Metadata } from "next";
+
+import "../[locale]/globals.css";
+
+export const metadata: Metadata = {
+  title: "Offline",
+  description: "Offline fallback page.",
+};
+
+export default function OfflineLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className="bg-background text-foreground min-h-dvh antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
